dc682e37b7e99b68751742bc508725421f9616b8
Tanya <Tania>
2024-08-25 20:31:20 +0200

    added function to monitor the food amount

--- src/monitor.c
+++ src/monitor.c
@@ -12,46 +12,70 @@
 
 #include "../include/philosophers.h"
 
-int	if_alive(t_philosopher *philo, t_simulation *sim)
+int meals_finished(t_simulation *sim)
 {
-	int	i;
-	long long	time_since_last_meal;
+    int i;
 
-	i = 0;
-	while (i < sim->number_of_philos)
-	{
-		pthread_mutex_lock(&philo->time_lock);
-		time_since_last_meal = current_time() - philo->last_meal_time;
-		if (time_since_last_meal > sim->time_to_die)
-		{
-			//block
-			philo->if_alive = DEAD;
-			pthread_mutex_unlock(&philo->time_lock);
-			//block
-			pthread_mutex_lock(sim->print_lock);
-			printf("\033[1;31m%lld %d died\033[0m\n", time_since_last_meal, philo->id);
-			pthread_mutex_unlock(sim->print_lock);
-			return (1);
-		}
-		pthread_mutex_unlock(&philo->time_lock);
-		i++;
-	}
-	return (0);
+    //pthread_mutex_lock(sim->state);
+    for (i = 0; i < sim->number_of_philos; i++)
+    {
+        if (sim->philos[i].meals_eaten < sim->number_of_meals)
+        {
+           // pthread_mutex_unlock(sim->state);
+            return (0);  // Not all philosophers have finished eating
+        }
+    }
+  // pthread_mutex_unlock(sim->state);
+    return (1);  // All philosophers have finished eating
+}
+
+int if_alive(t_philosopher *philo, t_simulation *sim)
+{
+    long long time_since_last_meal;
+
+    pthread_mutex_lock(&philo->time_lock);
+    time_since_last_meal = current_time() - philo->last_meal_time;
+    if (time_since_last_meal > sim->time_to_die)
+    {
+        pthread_mutex_lock(sim->state);
+        philo->if_alive = DEAD;
+        pthread_mutex_unlock(sim->state);
+        pthread_mutex_unlock(&philo->time_lock);
+        pthread_mutex_lock(sim->print_lock);
+        printf("\033[1;31m%lld %d died\033[0m\n", current_time(), philo->id);
+        pthread_mutex_unlock(sim->print_lock);
+        return (1);
+    }
+    pthread_mutex_unlock(&philo->time_lock);
+    return (0);
 }
 
 void *monitor(void *arg)
 {
-	int				i;
-	t_simulation	*sim;
+	int			i;
+    t_simulation *sim;
 
 	i = 0;
 	sim = (t_simulation *)arg;
-	while (1)
-	{
-		if (if_alive(&sim->philos[i], sim))
+    while (1)
+    {
+		i = 0;
+        while (i < sim->number_of_philos)
+        {
+            if (if_alive(&sim->philos[i], sim))
+            {
+                exit(0);  // Exit if a philosopher has died
+            }
+			i++;
+        }
+        if (sim->number_of_meals != 0)
 		{
-			exit (0);
+			if(meals_finished(sim))
+        	{
+        	    exit(0);  // Exit if all philosophers have finished eating
+        	}
 		}
-	}
-	return (NULL);
+ 		precise_sleep(100); //Small delay to prevent high CPU usage
+    }
+    return NULL;
 }
